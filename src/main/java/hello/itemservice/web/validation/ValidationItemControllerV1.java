1c3b8740d5f8b007fbb34390024fb38f5e03311f
DoHunee <jehgns1@egmail.com>
2024-09-10 11:14:25 +0900

    검증 직접 처리 - 개발

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -3,13 +3,19 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -17,6 +23,7 @@ public class ValidationItemControllerV1 {
 
     private final ItemRepository itemRepository;
 
+    // 모든 아이템 목록을 조회하고, 목록 페이지로 이동
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -24,6 +31,7 @@ public class ValidationItemControllerV1 {
         return "validation/v1/items";
     }
 
+    // 특정 아이템의 상세 정보를 조회하고, 상세 페이지로 이동
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
@@ -31,20 +39,59 @@ public class ValidationItemControllerV1 {
         return "validation/v1/item";
     }
 
+    // 아이템 추가 폼을 보여줌
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
         return "validation/v1/addForm";
     }
 
+    // 새 아이템을 추가하고, 추가된 아이템의 상세 페이지로 리다이렉트
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000원 ~ 1,000,000원 까지 허용합니다.");
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // 특정 필드의 범위를 넘어서는 검증 로직
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+            }
+            // else if (!StringUtils.isNumeric(item.getPrice().toString())) {
+            //     errors.put("price", "가격은 숫자만 입력해야 합니다.");
+            // }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (!errors.isEmpty()) {
+            log.info("errors={}", errors);
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
+        // 성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v1/items/{itemId}";
     }
 
+    // 아이템 수정 폼을 보여줌
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
@@ -52,6 +99,7 @@ public class ValidationItemControllerV1 {
         return "validation/v1/editForm";
     }
 
+    // 아이템을 수정하고, 수정된 아이템의 상세 페이지로 리다이렉트
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
@@ -59,4 +107,3 @@ public class ValidationItemControllerV1 {
     }
 
 }
-
