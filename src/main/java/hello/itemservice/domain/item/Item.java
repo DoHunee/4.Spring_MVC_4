b264ad67f1036dcdc61688647443ab94987cda62
DoHunee <jehgns1@egmail.com>
2024-09-12 15:34:45 +0900

    Bean Validation - 한계

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 
-import jakarta.validation.constraints.Max;
+// import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
    
@@ -15,6 +15,8 @@ import jakarta.validation.constraints.NotNull;
 // @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 // Java 15 이상에서는 JavaScript 엔진인 Nashorn이 제거되어 @ScriptAssert 어노테이션 오류가 발생한다
 public class Item {
+   
+    @NotNull //수정 요구사항 추가
     private Long id;
     
     @NotBlank (message = "공백 X")
@@ -25,7 +27,7 @@ public class Item {
     private Integer price;
     
     @NotNull
-    @Max(9999)
+    // @Max(9999)   //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
